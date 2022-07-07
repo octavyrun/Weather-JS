b37ad9aa646f37bd0e24f52575240e3b007c9cc8
Octavy Run <octavyrun@gmail.com>
2022-07-08 01:45:01 +0300

    Last commit

--- app.js
+++ app.js
@@ -30,7 +30,6 @@ function getWeather() {
   weather
     .getWeather()
     .then((results) => {
-      console.log(results);
       ui.paint(results);
     })
     .catch((err) => console.log(err));
